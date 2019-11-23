316849bbc86dbe72d582558d2cb9ae48f95e1f9b
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2019-11-23 15:15:15 +0100

    Added stats and mana to Minion object; changed test script to do multiple runs

--- scripts/minion.py
+++ scripts/minion.py
@@ -3,15 +3,17 @@ import random
 
 class Minion:
 
-    def __init__(self, player, attack=1, health=1, name=None):
+    def __init__(self, player, attack=1, health=1, name=None, stars=1, mana=1):
         self.attack = attack
         self.health = health
         self.player = player
+        self.stars = stars
+        self.mana = mana
         if name is None:
             self.name = random.random()
         else:
             self.name = name
-        print(f'Minion {self.name} with stats {self.attack},{self.health} created.')
+        #print(f'Minion {self.name} with stats {self.attack},{self.health} created.')
 
     def find_target(self):
         return self.player.get_opponent().get_defender()
@@ -20,20 +22,20 @@ class Minion:
         self.health -= damage
         if self.health <= 0:
             self.player.set_dead(self)
-            print(f'Minion {self.name} has died!')
+        #    print(f'Minion {self.name} has died!')
 
     def do_attack(self, minion):
         self.take_damage(minion.attack)
         minion.take_damage(self.attack)
-        print(f'Minion {self.name} has {self.health} health remaining. '
-              f'Opponent {minion.name} has {minion.health} health remaining.')
+        #print(f'Minion {self.name} has {self.health} health remaining. '
+        #      f'Opponent {minion.name} has {minion.health} health remaining.')
 
     def combat(self):
         target = self.find_target()
         self.do_attack(target)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     m1 = Minion(4, 3)
     m2 = Minion(1, 5)
     m1.do_attack(m2)

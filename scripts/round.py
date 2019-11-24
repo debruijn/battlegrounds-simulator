a5a34d0e5ccd9e4f3b087694c26745a4a31899cc
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2019-11-24 12:19:17 +0100

    Add unit tests for Round, make changes to Round because of that

--- scripts/round.py
+++ scripts/round.py
@@ -5,37 +5,49 @@ class Round:
 
     def __init__(self, players):
         self.players = players
+        self.current_player = 0
+        self.result = -1
+        self.set_players_as_opponents()
+
+    def set_players_as_opponents(self):
+        self.players[0].set_opponent(self.players[1])
+        self.players[1].set_opponent(self.players[0])
+
+    def select_first_player(self):
+        # Select first player based on minion count
+        if len(self.players[0].minions) > len(self.players[1].minions):
+            self.current_player = 0
+        elif len(self.players[0].minions) < len(self.players[1].minions):
+            self.current_player = 1
+        else:
+            if random.random() < 0.5:
+                self.current_player = 0
+            else:
+                self.current_player = 1
+
+    def find_result(self):
+        if self.players[0].check_alive():
+            # print("Player 1 has won!")
+            self.result = 0
+        elif self.players[1].check_alive():
+            # print("Player 2 has won!")
+            self.result = 1
+        else:
+            # print("It was a draw!")
+            self.result = 2
+        return self.result
 
     def run(self):
 
         # TODO: Copy players to allow for easy reruns. Need to investigate deep copies of objects
 
-        # Select first player based
-        if len(self.players[0].minions) > len(self.players[1].minions):
-            current_player = 0
-        elif len(self.players[0].minions) < len(self.players[1].minions):
-            current_player = 1
-        else:
-            if random.random() < 0.5:
-                current_player = 0
-            else:
-                current_player = 1
+        self.select_first_player()
 
         while self.players[0].check_alive() and self.players[1].check_alive():
-            self.players[current_player].do_attack()
-            current_player = 1 - current_player
+            self.players[self.current_player].do_attack()
+            self.current_player = 1 - self.current_player
 
-        if self.players[0].check_alive():
-            # print("Player 1 has won!")
-            result = 0
-        elif self.players[1].check_alive():
-            # print("Player 2 has won!")
-            result = 1
-        else:
-            # print("It was a draw!")
-            result = 2
-
-        return result
+        return self.find_result()
 
 
 if __name__ == "__main__":
@@ -48,9 +60,6 @@ if __name__ == "__main__":
 
     player2 = Player()
     player2.add_minion(Minion(player2, 3, 9))
-
-    player1.set_opponent(player2)
-    player2.set_opponent(player1)
 
     round = Round([player1, player2])
     print(round.run())
---
+++ tests/test_round.py
@@ -0,0 +1,82 @@
+import unittest
+from scripts.minion import Minion
+from scripts.player import Player
+from scripts.round import Round
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_set_players_as_opponents(self):
+        player1 = Player(minion=Minion())
+        player2 = Player(minion=Minion())
+        Round([player1, player2])
+        self.assertEqual(player1.opponent, player2)
+        self.assertEqual(player2.opponent, player1)
+
+    def test_select_first_player_based_on_minion_count(self):
+        player1 = Player(minions=[Minion() for __i in range(3)])
+        player2 = Player(minions=[Minion() for __i in range(2)])
+        round = Round([player1, player2])
+        round.select_first_player()
+        self.assertEqual(round.current_player, 0)
+
+    def test_select_first_player_based_on_minion_count_alternative(self):
+        player1 = Player(minions=[Minion() for __i in range(2)])
+        player2 = Player(minions=[Minion() for __i in range(3)])
+        round = Round([player1, player2])
+        round.select_first_player()
+        self.assertEqual(round.current_player, 1)
+
+    def test_select_first_player_equal_minion_count(self):
+        player1 = Player(minions=[Minion() for __i in range(3)])
+        player2 = Player(minions=[Minion() for __i in range(3)])
+        round = Round([player1, player2])
+        round.select_first_player()
+        self.assertIn(round.current_player, [0, 1])
+
+    def test_result_one_player_dead(self):
+        player1 = Player(minion=Minion())
+        player2 = Player(minion=Minion())
+        round = Round([player1, player2])
+        player1.set_dead(player1.minions[0])
+        self.assertEqual(round.find_result(), 1)
+
+    def test_result_one_player_dead_alternative(self):
+        player1 = Player(minion=Minion())
+        player2 = Player(minion=Minion())
+        round = Round([player1, player2])
+        player2.set_dead(player2.minions[0])
+        self.assertEqual(round.find_result(), 0)
+
+    def test_result_both_players_dead(self):
+        player1 = Player(minion=Minion())
+        player2 = Player(minion=Minion())
+        round = Round([player1, player2])
+        player1.set_dead(player1.minions[0])
+        player2.set_dead(player2.minions[0])
+        self.assertEqual(round.find_result(), 2)
+
+    def test_run_gives_winner(self):
+        player1 = Player(minions=[Minion() for __i in range(3)])
+        player2 = Player(minions=[Minion() for __i in range(2)])
+        round = Round([player1, player2])
+
+        self.assertEqual(round.run(), 0)
+
+    def test_run_gives_winner_alternative(self):
+        player1 = Player(minions=[Minion() for __i in range(2)])
+        player2 = Player(minions=[Minion() for __i in range(3)])
+        round = Round([player1, player2])
+
+        self.assertEqual(round.run(), 1)
+
+    def test_run_gives_draw(self):
+        player1 = Player(minions=[Minion() for __i in range(3)])
+        player2 = Player(minions=[Minion() for __i in range(3)])
+        round = Round([player1, player2])
+
+        self.assertEqual(round.run(), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
